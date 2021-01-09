77d6f13e6679af4558be7a10939d9e062c8a93a2
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2021-01-09 10:33:46 +0530

    initial commiy

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList , TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function App(){
+
+  const [datas,setData]=useState([
+    { title:'yash',id:'0' },
+    { title:'bash',id:'1' },
+   ])
+
+  const [name,setName]=useState('')
+  function preview(dummy)
+  {
+    if(dummy.name)
+    { 
+      return (
+        <View>
+          <Text> {dummy.name} </Text>
+        </View>
+      )
+    }
+    else {
+      return (
+        <View>
+          <Text> please enter the name</Text>
+        </View>
+      )
+    }
+  }
+  
+
+  function action(id){
+   console.log(id)
+
+  }
+
+  return(
+    <View  style={Styles.container}> 
+      <Text style={{ paddingTop:190,margin:10,fontSize:50 }}  > Todo App </Text>
+      
+      <Text> preview :- { preview({name}) } </Text>
+      
+      <TextInput style={Styles.input} class="some" value={name}  onChangeText={ (text) => (setName(text))} 
+      />
+     
+      <Button title="submit" onPress={()=> 
+        (
+          setData([...datas,{
+              title:name,
+              id:datas.length,
+            }]),
+            setName('')
+          )} 
+      />
+
+  <FlatList 
+       
+        keyExtractor={(item) => item.id.toString()} 
+        data={datas} 
+        renderItem={({ item }) => ( 
+          <TouchableOpacity onPress={() => (action(item.id)) } > 
+            <Text>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
+
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
+
+
+const Styles = StyleSheet.create({
+
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+
+  input:{
+    width:230,
+    margin:20,
+    padding:5,
+    borderWidth:1,
+    borderColor:'grey',
+    textAlign:'center',
+  }
+
+
+})
+
